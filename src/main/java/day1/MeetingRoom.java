6bf59cb47193581a040ad71baf3aa7259051b50c
hoyaspark <hoyaspark@gmail.com>
2022-05-01 01:24:13 +0900

    알고리즘 문제 추가

---
+++ src/main/java/day1/MeetingRoom.java
@@ -0,0 +1,73 @@
+package day1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class MeetingRoom {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Item> items = new ArrayList<>();
+        int count = scanner.nextInt();
+        int time = 0;
+        int result = 0;
+
+        for (int i = 0; i < count; i++) {
+            int startTime = scanner.nextInt();
+            int endTime = scanner.nextInt();
+            items.add(new Item(startTime, endTime));
+        }
+
+        items.sort((a, b) -> {
+            if (a.getEndTime() == b.getEndTime()) {
+                return a.getStartTime() - b.getStartTime();
+            }
+            return a.getEndTime() - b.getEndTime();
+        });
+
+        for (int i = 0; i < count; i++) {
+            if (i == 0) {
+                time = items.get(0).getEndTime();
+                result++;
+                continue;
+            }
+
+            if (items.get(i).getStartTime() >= time) {
+                result++;
+                time = items.get(i).getEndTime();
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+    private static class Item {
+        private int startTime;
+        private int endTime;
+
+        public Item(int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public int getStartTime() {
+            return startTime;
+        }
+
+        public int getEndTime() {
+            return endTime;
+        }
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "startTime=" + startTime +
+                    ", endTime=" + endTime +
+                    '}';
+        }
+    }
+}
