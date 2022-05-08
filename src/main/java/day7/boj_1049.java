9c3fde8cd5dbbc10f21b5c9db7688d91cda53a6f
hoyaspark <hoyaspark@gmail.com>
2022-05-08 23:56:45 +0900

    알고리즘 문제 추가 (Day-7)

---
+++ src/main/java/day7/boj_1049.java
@@ -0,0 +1,46 @@
+package day7;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class boj_1049 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        ArrayList<Integer> setList = new ArrayList<>();
+        ArrayList<Integer> oneList = new ArrayList<>();
+
+
+        for (int i = 0; i < M; i++) {
+            int set = scanner.nextInt();
+            int one = scanner.nextInt();
+            setList.add(set);
+            oneList.add(one);
+        }
+
+        setList.sort((a, b) -> a - b);
+        oneList.sort((a, b) -> a - b);
+
+        PriorityQueue<Integer> result = new PriorityQueue<>((a, b) -> a - b);
+
+        int sum = 0; // 세트 + 낱개
+        sum += setList.get(0) * (N / 6); // 세트
+        sum += oneList.get(0) * (N % 6); // 낱개
+        result.add(sum);
+
+        // 세트로만
+        result.add(setList.get(0) * ((N / 6) + 1));
+
+        // 낱개로만
+        result.add(oneList.get(0) * N);
+
+        System.out.println(result.poll());
+
+    }
+
+}
---
+++ src/main/java/day7/boj_2217.java
@@ -0,0 +1,33 @@
+package day7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class boj_2217 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        list.sort((a, b) -> a - b);
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            result.add(list.get(i) * (N - i));
+        }
+
+        result.sort((a, b) -> b - a);
+
+        System.out.println(result.get(0));
+
+    }
+
+}
