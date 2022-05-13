508168da688b2c2f59caf00b408213578fb35bb0
박상호 <hoyaspark@gmail.com>
2022-05-13 21:03:40 +0900

    feat : 알고리즘 문제 추가 (알고리즘 기초 (1/2) - dp,수학)

---
+++ src/main/java/boj400/boj_16194.java
@@ -0,0 +1,36 @@
+package boj400;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj_16194 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] p = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            p[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[n + 1];
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        dp[1] = p[1];
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + p[j]);
+                /**
+                 * dp[2] = dp[2-1],p[1] <> dp[2-2],p[2]
+                 */
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
---
+++ src/main/java/boj400/boj_9095.java
@@ -0,0 +1,34 @@
+package boj400;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class boj_9095 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        dp[4] = 7;
+
+        for (int i = 5; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(dp[list.get(i)]);
+        }
+    }
+
+}
