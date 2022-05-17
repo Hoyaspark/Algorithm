9558ee62b8352a0da70af9d76e436d0a11acedee
박상호 <hoyaspark@gmail.com>
2022-05-17 23:03:16 +0900

    feat : 알고리즘 문제 추가 (DP)

---
+++ src/main/java/boj401/boj_11054.java
@@ -0,0 +1,53 @@
+package boj401;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj_11054 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n + 1];
+        int[] dp1 = new int[n + 1];
+        int[] dp2 = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.fill(dp1, 1);
+        Arrays.fill(dp2, 1);
+
+        // 오름차순 가장 긴 수열
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp1[i] = Math.max(dp1[i], dp1[j] + 1);
+                }
+            }
+        }
+
+        // 내림차순 가장 긴 수열
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                if (arr[i] > arr[j]) {
+                    dp2[i] = Math.max(dp2[i], dp2[j] + 1);
+                }
+            }
+        }
+
+        int[] result = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            result[i] = dp1[i] + dp2[i] - 1;
+        }
+
+        int max = Arrays.stream(result).max().getAsInt();
+
+        System.out.println(max);
+
+    }
+}
---
+++ src/main/java/boj401/boj_11722.java
@@ -0,0 +1,32 @@
+package boj401;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class boj_11722 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
